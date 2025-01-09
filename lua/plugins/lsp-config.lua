6d5700caa680b1aee40ec038f9f86aece710a761
Yosel Del Valle <yosel.del.valle@a8c.com>
2025-01-08 22:30:54 -0500

    Add autocomplete. Let lsp to automatically load servers

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,24 +1,35 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
     end
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" }
-      })
-    end
+    lazy = false,
+    opts = {
+      auto_install = true
+    }
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.ruby_lsp.setup({
+        capabilities = capabilities
+      })
       vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, {})
       vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
       vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
