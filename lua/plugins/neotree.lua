35c95db94eef178ef35b03118889073b13347c90
Yosel Del Valle <yosel.del.valle@a8c.com>
2025-01-29 21:23:55 -0500

    Use keybindings with control key instead

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -7,6 +7,6 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<D-b>', ':Neotree filesystem reveal right<CR>')	
+    vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle left<CR>')
   end
 }
